fe36c42e90e45040bf1440a0dbf3960d9aeef59a
rafi_syaamil <62317990+rsyaamil@users.noreply.github.com>
2022-03-07 01:25:23 +0700

    Add files via upload

---
+++ variable_user.php
@@ -0,0 +1,13 @@
+<?php
+// definisikan variables
+    $nama = 'Muhammad Rafi Syaamil';
+    $umur = 21;
+    $berat = 55;
+
+    echo 'Nama : ' . $nama;
+    echo '<br/>Umur : ' . $umur.' Tahun';
+    echo '<br/>Berat : '.$berat.' Kg';
+
+    echo "<br/>Hello $nama Apa Kabar";
+
+    ?>
